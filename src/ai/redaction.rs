aae907816477ac2bb9302aa82bc5aa901261453f
jhoninck <jan.honinckx@infochain.be>
2026-03-13 10:25:30 +0100

    Implement Redactor struct with clean method

--- src/ai/redaction.rs
+++ src/ai/redaction.rs
@@ -1,1 +1,17 @@
+pub struct Redactor {}
 
+impl Redactor {
+
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn clean(&self, input: &str) -> String {
+
+        let mut text = input.to_string();
+
+        text = text.replace("@", "[at]");
+
+        text
+    }
+}
